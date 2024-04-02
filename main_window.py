afd28435fd12f07439b5a17109ed9ead6ddcb3d8
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-02 17:57:49 +0200

    Added main window with useless side button to the storyline

--- main_window.py
+++ main_window.py
@@ -1,8 +1,17 @@
 import sys
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
 
 class MainWindow(QMainWindow):
     def __init__(self, app: QApplication):
         super().__init__()
 
         self.app: QApplication = app
+
+        self.button = QPushButton("Push me", self)
+        self.button.released.connect(self.print_txt)
+        self.add_widget(self.button)
+
+    @Slot()
+    def print_txt(self):
+        print("Slot")
