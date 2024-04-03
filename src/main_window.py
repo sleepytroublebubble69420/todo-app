cc32c0e12038ee57bccb8623f23541e431403f37
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-04 00:16:15 +0200

    Undo last commit lol because I used it wrongly x_x

--- src/main_window.py
+++ src/main_window.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot, QSize
 from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
 
 
@@ -9,10 +9,9 @@ class MainWindow(QMainWindow):
         self.app: QApplication = app
 
         self.button: QPushButton = QPushButton("Push me", self)
-        button = self.button
-        button.sizeHint(100, 60)
+        button: QPushButton = self.button
         button.released.connect(self.print_txt)
-        self.setCentralWidget(self.button)
+        self.setCentralWidget(button)
 
     @Slot()
     def print_txt(self):
