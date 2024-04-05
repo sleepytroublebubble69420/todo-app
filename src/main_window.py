485f79936676acb920aaa83cf5980b96ea6cdb33
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-06 01:21:44 +0200

    Figured out layouts and how to use central wigdet of main window!

--- src/main_window.py
+++ src/main_window.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QHBoxLayout
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QHBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
@@ -7,19 +7,22 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.app: QApplication = app
-        self.setLayout(QHBoxLayout(self))
+        centralWidget: QWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
 
+        layout: QHBoxLayout = QHBoxLayout(centralWidget)
+        centralWidget.setLayout(QHBoxLayout(centralWidget))
         self.button1: QPushButton = QPushButton("Push me", self)
         button: QPushButton = self.button1
         button.setFixedSize(100, 60)
-        self.setCentralWidget(button)
+        layout.addWidget(button)
 
         self.button2: QPushButton = QPushButton("Push me", self)
         button: QPushButton = self.button2
         button.setFixedSize(100, 60)
-        self.setCentralWidget(button)
+        layout.addWidget(button)
 
         self.button3: QPushButton = QPushButton("Push me", self)
         button: QPushButton = self.button3
         button.setFixedSize(100, 60)
-        self.setCentralWidget(button)
+        layout.addWidget(button)
