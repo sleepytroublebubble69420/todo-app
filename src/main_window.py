c432abbb7e3afe9cadaf586ebc5ef83a6ee6abd0
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-05 22:45:00 +0200

    Trying to figure out how to layout and use size polocies

--- src/main_window.py
+++ src/main_window.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton
+from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QHBoxLayout
 
 
 class MainWindow(QMainWindow):
@@ -7,12 +7,19 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.app: QApplication = app
+        self.setLayout(QHBoxLayout(self))
 
-        self.button: QPushButton = QPushButton("Push me", self)
-        button: QPushButton = self.button
-        button.released.connect(self.print_txt)
+        self.button1: QPushButton = QPushButton("Push me", self)
+        button: QPushButton = self.button1
+        button.setFixedSize(100, 60)
         self.setCentralWidget(button)
 
-    @Slot()
-    def print_txt(self):
-        print("Slot")
+        self.button2: QPushButton = QPushButton("Push me", self)
+        button: QPushButton = self.button2
+        button.setFixedSize(100, 60)
+        self.setCentralWidget(button)
+
+        self.button3: QPushButton = QPushButton("Push me", self)
+        button: QPushButton = self.button3
+        button.setFixedSize(100, 60)
+        self.setCentralWidget(button)
