99f6a249c4ce81dded53023e6bf41022df7532ce
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-06 10:58:23 +0200

    Replaced central widget of main window by my custom TodoList widget

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
-from todo_list import TodoList
 
 
 def main():
--- src/main_window.py
+++ src/main_window.py
@@ -2,27 +2,11 @@ from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QHBoxLayout, QWidget
 from todo_list import TodoList
 
+
 class MainWindow(QMainWindow):
     def __init__(self, app: QApplication):
         super().__init__()
 
         self.app: QApplication = app
-        centralWidget: QWidget = QWidget(self)
+        centralWidget: TodoList = TodoList(self)
         self.setCentralWidget(centralWidget)
-
-        layout: QHBoxLayout = QHBoxLayout(centralWidget)
-        centralWidget.setLayout(QHBoxLayout(centralWidget))
-        self.button1: QPushButton = QPushButton("Push me", self)
-        button: QPushButton = self.button1
-        button.setFixedSize(100, 60)
-        layout.addWidget(button)
-
-        self.button2: QPushButton = QPushButton("Push me", self)
-        button: QPushButton = self.button2
-        button.setFixedSize(100, 60)
-        layout.addWidget(button)
-
-        self.button3: QPushButton = QPushButton("Push me", self)
-        button: QPushButton = self.button3
-        button.setFixedSize(100, 60)
-        layout.addWidget(button)
