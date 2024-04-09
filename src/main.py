9bc367565acac5cdb64cf9f2ca966b0978f3f796
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-09 22:46:04 +0200

    Cleaned useless lines in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import sys
-from PySide6.QtWidgets import QApplication, QListWidgetItem
+from PySide6.QtWidgets import QApplication
 from main_window import MainWindow
 from todo_list import TodoList
-from todo import Todo
 
 
 def main():
