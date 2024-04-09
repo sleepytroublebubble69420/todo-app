15b7bd5bacf896b32ab433335abd2c612c359db9
sleepytroublebubble69420 <sleepytroublebubble@proton.me>
2024-04-10 00:52:50 +0200

    Reverted changes in last commit and swapped todos' check boxes by button

--- src/todo.py
+++ src/todo.py
@@ -1,22 +1,21 @@
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QCheckBox, QLineEdit
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLabel
 from PySide6.QtCore import Slot
 
 
 class Todo(QWidget):
-    def __init__(self, todo_name = "Todo", parent = None):
+    def __init__(self, label = "Todo", parent = None):
         super().__init__(parent)
 
         layout: QHBoxLayout = QHBoxLayout()
         self.setLayout(layout)
 
-        self.todo_name: QLineEdit = QLineEdit(todo_name)
-        layout.addWidget(self.todo_name)
+        self.label: QLabel = QLabel(label)
+        layout.addWidget(self.label)
 
-        self.check_box: QCheckBox = QCheckBox()
-        layout.addWidget(self.check_box)
-        self.check_box.stateChanged.connect(self.on_box_checked)
+        self.button: QPushButton = QPushButton()
+        layout.addWidget(self.button)
+        self.button.clicked.connect(self.on_box_checked)
 
     @Slot()
-    def on_box_checked(self, state):
-        print(self.todo_name.text())
-        print(state)
+    def on_box_checked(self):
+        print(self.label.text())
